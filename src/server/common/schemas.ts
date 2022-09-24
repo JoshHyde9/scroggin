aa535c244b6b9878b4cef8647779beb666d38615
JoshHyde9 <joshhyde546@gmail.com>
2022-09-24 12:29:03 +1000

    fix: changed tags to string and will convert to string array on backend

--- src/server/common/schemas.ts
+++ src/server/common/schemas.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { TypeOf, z } from "zod";
 
 export const loginSchema = z.object({
   email: z.string().email(),
@@ -14,12 +14,16 @@ export const registerSchema = loginSchema.extend({
 export type ILogin = z.infer<typeof loginSchema>;
 export type IRegister = z.infer<typeof registerSchema>;
 
-export const recipeSchema = z.object({
+export const createRecipeSchema = z.object({
   name: z.string().min(1),
   ingredients: z.string().min(1),
   method: z.string().min(1),
-  tags: z.array(z.string()).nonempty(),
+  tags: z.string().min(1),
+});
+
+export const recipeSchema = createRecipeSchema.extend({
   likeCount: z.number(),
 });
 
+export type ICreateRecipe = z.infer<typeof createRecipeSchema>;
 export type IRecipe = z.infer<typeof recipeSchema>;
