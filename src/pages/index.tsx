460714e37ef9fb9b3bdea91e136b7c0fed084624
JoshHyde9 <joshhyde546@gmail.com>
2022-09-24 20:11:42 +1000

    feat: display all recipes from db

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import type { NextPage } from "next";
-import { signOut, useSession } from "next-auth/react";
+import RecipeCard from "../components/RecipeCard";
+import { IRecipe } from "../server/common/schemas";
+import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  const { data: session } = useSession();
+  const { data: recipes, isLoading } = trpc.useQuery(["recipe.getAll"]);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!recipes) {
+    return <h1>No recipes found!</h1>;
+  }
+
   return (
-    <>
-      <header className="flex justify-center mt-5">
-        <h1 className="text-4xl semi-bold">Hello World!</h1>
-      </header>
-    </>
+    <div className="grid grid-cols-1 gap-10 px-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {recipes.map((recipe: IRecipe) => {
+        return <RecipeCard recipe={recipe} />;
+      })}
+    </div>
   );
 };
 
