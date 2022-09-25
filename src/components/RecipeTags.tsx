bde0109f80debcd06caab2aa05e9b50b27479cb3
JoshHyde9 <joshhyde546@gmail.com>
2022-09-25 12:59:41 +1000

    refactor: moved recipe tags into own component

---
+++ src/components/RecipeTags.tsx
@@ -0,0 +1,21 @@
+interface Props {
+  tags: string[];
+  classes?: string;
+}
+
+export const RecipeTags: React.FC<Props> = ({ tags, classes }: Props) => {
+  return (
+    <div className={`flex gap-2 mt-2 ${classes}`}>
+      {tags.map((tag: string) => {
+        return (
+          <p
+            key={tag}
+            className="px-4 py-0.5 bg-purple-500/50 border border-purple-700 text-sm rounded-full"
+          >
+            {tag}
+          </p>
+        );
+      })}
+    </div>
+  );
+};
