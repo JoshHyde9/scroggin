cef5a6a6b9d52ecc829cc2b8126df8ec847ffdee
JoshHyde9 <joshhyde546@gmail.com>
2022-09-27 20:51:21 +1000

    feat: added like recipe schema

--- src/server/common/schemas.ts
+++ src/server/common/schemas.ts
@@ -38,3 +38,10 @@ export const recipeSchema = frontendCreateRecipeSchema.extend({
 export type ICreateFontendRecipe = z.infer<typeof frontendCreateRecipeSchema>;
 export type ICreateBackendRecipe = z.infer<typeof backendCreateRecipeSchema>;
 export type IRecipe = z.infer<typeof recipeSchema>;
+
+export const likeSchema = z.object({
+  userId: z.string().min(1),
+  recipeId: z.string().min(1),
+  value: z.number(),
+});
+export type ILike = z.infer<typeof likeSchema>;
