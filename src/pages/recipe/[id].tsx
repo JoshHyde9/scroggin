813d1a4f0abdc29285a50c70e417c0bd8e6f6322
JoshHyde9 <joshhyde546@gmail.com>
2022-09-27 20:57:44 +1000

    feat: heart icon submits a like

--- src/pages/recipe/[id].tsx
+++ src/pages/recipe/[id].tsx
@@ -25,11 +25,18 @@ const RecipePage: NextPage = ({
   recipeCreator,
   userSession,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const utils = trpc.useContext();
   const router = useRouter();
 
   const { mutate: deleteRecipe } = trpc.useMutation(["recipe.delete"], {
     onSuccess: () => {
       router.push("/");
+    },
+  });
+
+  const { mutate: likePost } = trpc.useMutation(["recipe.like"], {
+    onSuccess: () => {
+      utils.invalidateQueries(["recipe.getByID", { id: recipe!.id }]);
     },
   });
 
@@ -52,19 +59,21 @@ const RecipePage: NextPage = ({
       <div className="max-w-md mx-auto my-2">
         <div className="flex justify-between gap-2">
           <div className="flex gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-            </svg>
+            <button onClick={() => likePost({ id: recipe.id, value: 1 })}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+              </svg>
+            </button>
             <p>{recipe.likeCount}</p>
           </div>
           <p>{dayjs(recipe.createdAt.toString()).format("DD/MM/YYYY HH:mm")}</p>
