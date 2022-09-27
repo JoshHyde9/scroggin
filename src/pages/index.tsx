842d9dbd75e5fbe3f044a99cdc5d7e18c5ed5edd
JoshHyde9 <joshhyde546@gmail.com>
2022-09-27 20:55:20 +1000

    feat: heart is coloured if the looged in user has liked the recipe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
-import type { NextPage } from "next";
+import type { NextPage } from "next/types";
 import { IRecipe } from "../server/common/schemas";
 import { trpc } from "../utils/trpc";
 
 import { RecipeCard } from "../components/RecipeCard";
+import { useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
   const { data: recipes, isLoading } = trpc.useQuery(["recipe.getAll"]);
+
+  const { data: userSession } = useSession();
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -15,10 +18,33 @@ const Home: NextPage = () => {
     return <h1>No recipes found!</h1>;
   }
 
+  const { data: likedRecipes } = trpc.useQuery([
+    "recipe.getUserLikes",
+    { userId: userSession?.user?.id },
+  ]);
+
+  if (!userSession || !userSession.user || !likedRecipes) {
+    return (
+      <div className="grid grid-cols-1 gap-10 px-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {recipes.map((recipe: IRecipe) => {
+          return <RecipeCard key={recipe.id} recipe={recipe} />;
+        })}
+      </div>
+    );
+  }
+
   return (
     <div className="grid grid-cols-1 gap-10 px-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {recipes.map((recipe: IRecipe) => {
-        return <RecipeCard key={recipe.id} recipe={recipe} />;
+      {recipes.map((recipe: IRecipe, i) => {
+        if (userSession.user!.id === likedRecipes[i]!.userId) {
+          return (
+            <RecipeCard
+              key={recipe.id}
+              recipe={recipe}
+              hasLoggedInUserLiked={true}
+            />
+          );
+        }
       })}
     </div>
   );
