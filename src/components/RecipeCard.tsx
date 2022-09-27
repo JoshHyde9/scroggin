5b30d8619a7e404f05f76531d8daeaa94f0e2354
JoshHyde9 <joshhyde546@gmail.com>
2022-09-27 20:57:10 +1000

    feat: heart icon submits a like

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -2,15 +2,26 @@ import dayjs from "dayjs";
 import Image from "next/image";
 import NextLink from "next/link";
 import { IRecipe } from "../server/common/schemas";
+import { trpc } from "../utils/trpc";
 import { RecipeTags } from "./RecipeTags";
 
 interface RecipeCardProps {
   recipe: IRecipe;
+  hasLoggedInUserLiked?: boolean;
 }
 
 export const RecipeCard: React.FC<RecipeCardProps> = ({
   recipe: { id, name, tags, likeCount, createdAt, displayImage },
+  hasLoggedInUserLiked,
 }: RecipeCardProps) => {
+  const utils = trpc.useContext();
+
+  const { mutate: likePost } = trpc.useMutation(["recipe.like"], {
+    onSuccess: () => {
+      utils.invalidateQueries(["recipe.getByID", { id }]);
+    },
+  });
+
   return (
     <div className="h-52 md:h-64 lg:h-80">
       <NextLink href={`/recipe/${id}`}>
@@ -28,19 +39,28 @@ export const RecipeCard: React.FC<RecipeCardProps> = ({
         <RecipeTags tags={tags} />
         <div className="flex justify-between gap-2 mt-2">
           <div className="flex gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
+            <button
+              onClick={() =>
+                likePost({ id, value: hasLoggedInUserLiked ? 1 : -1 })
+              }
             >
-              <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className={
+                  hasLoggedInUserLiked ? "stroke-red-500 fill-red-500" : ""
+                }
+              >
+                <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+              </svg>
+            </button>
             <p>{likeCount}</p>
           </div>
           <p>{dayjs(createdAt.toString()).format("DD/MM/YYYY HH:mm")}</p>
