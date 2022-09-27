76cea007888aaafc63b8f094e6dd58760852e3ea
JoshHyde9 <joshhyde546@gmail.com>
2022-09-28 09:05:37 +1000

    fix: SSR page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,32 @@
-import type { NextPage } from "next/types";
+import type {
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+  NextPage,
+  PreviewData,
+} from "next/types";
+import { ParsedUrlQuery } from "querystring";
+
+import { prisma } from "../server/db/client";
 import { IRecipe } from "../server/common/schemas";
-import { trpc } from "../utils/trpc";
+import { getServerAuthSession } from "../server/common/get-server-auth-session";
 
 import { RecipeCard } from "../components/RecipeCard";
-import { useSession } from "next-auth/react";
 
-const Home: NextPage = () => {
-  const { data: recipes, isLoading } = trpc.useQuery(["recipe.getAll"]);
-
-  const { data: userSession } = useSession();
-
-  if (isLoading) {
-    return <h1>Loading...</h1>;
-  }
-
+const Home: NextPage<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> = ({
+  recipes,
+  userSession,
+  likedRecipes,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   if (!recipes) {
-    return <h1>No recipes found!</h1>;
+    return <div>No recipes found!</div>;
   }
 
-  const { data: likedRecipes } = trpc.useQuery([
-    "recipe.getUserLikes",
-    { userId: userSession?.user?.id },
-  ]);
-
-  if (!userSession || !userSession.user || !likedRecipes) {
+  if (!userSession || !likedRecipes || likedRecipes.length <= 0) {
     return (
       <div className="grid grid-cols-1 gap-10 px-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {recipes.map((recipe: IRecipe) => {
+        {recipes.map((recipe: IRecipe, i) => {
           return <RecipeCard key={recipe.id} recipe={recipe} />;
         })}
       </div>
@@ -48,6 +48,38 @@ const Home: NextPage = () => {
       })}
     </div>
   );
+};
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext<ParsedUrlQuery, PreviewData>
+) => {
+  const userSession = await getServerAuthSession(context);
+
+  let recipes = await prisma.recipe.findMany();
+
+  recipes = JSON.parse(JSON.stringify(recipes));
+
+  if (!userSession?.user) {
+    return {
+      props: {
+        recipes,
+      },
+    };
+  }
+
+  let likedRecipes = await prisma.like.findMany({
+    where: { userId: userSession.user.id },
+  });
+
+  likedRecipes = JSON.parse(JSON.stringify(likedRecipes));
+
+  if (!likedRecipes) {
+    return { props: { recipes, userSession } };
+  }
+
+  return {
+    props: { recipes, userSession, likedRecipes },
+  };
 };
 
 export default Home;
