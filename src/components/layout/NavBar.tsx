61654fbd477289bafcf5b2b9f7bd419d6ab622f4
JoshHyde9 <joshhyde546@gmail.com>
2022-10-01 12:02:21 +1000

    fix: navbar being weird with images

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -34,7 +34,7 @@ const MobileNav = ({
 
   return (
     <div
-      className={`absolute top-0 left-0 h-screen w-screen bg-slate-200 transform ${
+      className={`absolute top-0 left-0 h-screen w-screen bg-slate-200 z-10 transform ${
         open ? "-translate-x-0" : "-translate-x-full"
       } transition-transform duration-300 ease-in-out filter drop-shadow-md `}
     >
