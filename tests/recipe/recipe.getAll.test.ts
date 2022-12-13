83e8636d9af4f3fcb70fc36d571a266d584c25ba
JoshHyde9 <joshhyde546@gmail.com>
2022-12-14 08:59:35 +1100

    feat: started integration testing

---
+++ tests/recipe/recipe.getAll.test.ts
@@ -0,0 +1,30 @@
+import { Recipe } from "@prisma/client";
+import { describe, expect, it } from "vitest";
+import { appRouter } from "../../src/server/router";
+import { createContextInner } from "../../src/server/router/context";
+
+describe("recipe@getAll", async () => {
+  const ctx = await createContextInner({ session: null });
+  const caller = appRouter.createCaller(ctx);
+
+  const recipes = await caller.query("recipe.getAll");
+
+  it("should get an array of recipes with valid keys", () => {
+    expect(recipes).toEqual(
+      expect.arrayContaining<Recipe>([
+        expect.objectContaining({
+          id: expect.any(String),
+          name: expect.any(String),
+          displayImage: expect.any(String),
+          ingredients: expect.any(String),
+          method: expect.any(String),
+          tags: expect.any(Array<string>),
+          likeCount: expect.any(Number),
+          userId: expect.any(String),
+          createdAt: expect.any(Date),
+          updatedAt: expect.any(Date),
+        }),
+      ])
+    );
+  });
+});
